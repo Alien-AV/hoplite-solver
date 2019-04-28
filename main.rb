567e2c07667175658b3dd79ec9ddb17a517cfd43
Alien_AV <alienav@gmail.com>
2019-04-28 22:01:41 +0300

    moving stuff around

--- main.rb
+++ main.rb
@@ -1,8 +1,17 @@
-class MovingObject
-  attr_accessor :type
-  def initialize(type, x, y)
-    @type, @x, @y = type, x, y
+class Coord
+  attr_accessor :x, :y
+  def initialize(x,y)
+    @x,@y = x,y
   end
+end
+
+class MovingObject
+  attr_accessor :type, :coord
+  def initialize(type)
+    @type = type
+    @coord = nil
+  end
+
   WARRIOR_TYPE=4
   ARCHER_TYPE=5
   BOMBER_TYPE=6
@@ -12,20 +21,18 @@ class MovingObject
 end
 
 class Hex
-  attr_accessor :type
-  attr_accessor :contained_object
+  attr_accessor :type, :coord, :contained_object
   FLOOR_TYPE=0
   LAVA_TYPE=1
   EXIT_TYPE=2
   ALTAR_TYPE=3
 
-  def initialize(type, x, y)
-    @type, @x, @y = type, x, y
+  def initialize(type, coord)
+    @type, @coord = type, coord
     @contained_object = nil
   end
   def to_s
-    "#{@x},#{@y}:#{@type.to_s}"
-    # "#{@id.to_s}"
+    "#{@coord.x},#{@coord.y}:#{@type.to_s}"
   end
 end
 
@@ -49,11 +56,25 @@ class Position
   def initialize
     lineLengths = (7..11).to_a + (7..10).to_a.reverse
     @hexArray = lineLengths.each_with_index.map do | len, x |
-      Array.new(len) {|y| Hex.new(0, x, y)}
+      Array.new(len) {|y| Hex.new(0, Coord.new(x,y))}
     end
-    @player_location = [4,2]
-    @hexArray[@player_location[0]][@player_location[1]].type = Hex::PLAYER_TYPE
+    @player_location = Coord.new(4,2)
+    place_player(@player_location)
   end
+
+  def move_moving_object(moving_object, new_coord)
+    if moving_object.coord != nil
+      @hexArray[moving_object.coord.x][moving_object.coord.y] = nil
+    end
+    moving_object.coord = new_coord
+    @hexArray[moving_object.coord.x][moving_object.coord.y] = moving_object
+  end
+
+  def place_player(coord)
+    move_moving_object(MovingObject.new(MovingObject::PLAYER_TYPE), coord)
+    @player_location = coord
+  end
+
   def to_s
     @hexArray.each do | line |
       print "     " * (12 - line.size)
@@ -64,18 +85,17 @@ class Position
       puts
     end
   end
-  def neighbors(x,y)
-    # if adjacent line is longer, make a neighbor with y+1, and a neighbor with y
-    # if shorter, make a neighbor with y-1 and a neighbor with y
-    # always make a neighbor with
 
+  def neighbors(coords)
+    x,y = coords.x, coords.y
     neighbors = [[x, y+1], [x, y-1]]
     [-1,1].each do | x_off |
-      if(x+x_off >= 0 && x+x_off < @hexArray.size)
-        if(@hexArray[x+x_off].size > @hexArray[x].size)
-          neighbors += [[x+x_off, y], [x+x_off, y+1]]
+      neighbor_x = x + x_off
+      if neighbor_x.between?(0, @hexArray.size-1)
+        if(@hexArray[neighbor_x].size > @hexArray[x].size)
+          neighbors += [[neighbor_x, y], [neighbor_x, y+1]]
         else
-          neighbors += [[x+x_off, y-1], [x+x_off, y]]
+          neighbors += [[neighbor_x, y-1], [neighbor_x, y]]
         end
       end
     end
@@ -84,8 +104,9 @@ class Position
       x >= 0 && x < @hexArray.size && y >= 0 && y <= @hexArray[x].size
     end
   end
+
   def playerNeighbors
-    neighbors(*@player_location)
+    neighbors(@player_location)
   end
 
   def generatePotentialPositions
