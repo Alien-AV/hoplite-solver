e9532159767cbcb7b7d2143d6943f9c883b7f11c
Alien_AV <alienav@gmail.com>
2019-04-29 05:15:48 +0300

    create visual representation for the board

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'ruby2d'
+
 class Coord
   attr_accessor :x, :y
   def initialize(x,y)
@@ -9,14 +11,13 @@ class Coord
 end
 
 class MovingObject
-  attr_accessor :type, :coord
+  attr_accessor :type, :containing_hex
   def initialize(type)
     @type = type
-    @coord = nil
   end
 
   def to_s
-    "MovingObject"
+    "object: #{@type} in #{@containing_hex}"
   end
 
   WARRIOR_TYPE=4
@@ -39,7 +40,7 @@ class Hex
     @contained_object = nil
   end
   def to_s
-    "#{@coord.x},#{@coord.y}:#{@type.to_s}"
+    "#{@coord.x},#{@coord.y}:#{@type.to_s}#{@contained_object ? @contained_object.type : 0}"
   end
 end
 
@@ -65,49 +66,111 @@ end
 POTENTIAL_MOVE_TYPES = [Step, :lunge, :bash, :throw]
 
 class Position
-  def initialize
-    lineLengths = (7..11).to_a + (7..10).to_a.reverse
-    @hexArray = lineLengths.each_with_index.map do | len, x |
-      Array.new(len) {|y| Hex.new(0, Coord.new(x,y))}
+  attr_accessor :player
+  def initialize(board_state_dsl_representation = nil)
+    if(board_state_dsl_representation)
+      @hexArray = board_state_dsl_representation.each_with_index.map do | column_string, x |
+        column_string.chars.each_with_index.map do | hex_char, y |
+          case hex_char
+          when "F"
+            Hex.new(Hex::FLOOR_TYPE, Coord.new(x,y))
+          when "L"
+            Hex.new(Hex::LAVA_TYPE, Coord.new(x,y))
+          when "P"
+            hex = Hex.new(Hex::FLOOR_TYPE, Coord.new(x,y))
+            @player = MovingObject.new(MovingObject::PLAYER_TYPE)
+            place_object(@player, hex)
+            hex
+          when "E"
+            Hex.new(Hex::EXIT_TYPE, Coord.new(x,y))
+          else
+            raise "unknown char"
+          end
+        end
+      end
+      raise "no exit at its place" unless @hexArray[4][9].type == Hex::EXIT_TYPE
+    else
+      lineLengths = (7..11).to_a + (7..10).to_a.reverse
+      @hexArray = lineLengths.each_with_index.map do | len, x |
+        Array.new(len) {|y| Hex.new(Hex::FLOOR_TYPE, Coord.new(x,y))}
+      end
+      @player = MovingObject.new(MovingObject::PLAYER_TYPE)
+      place_object(@player, Coord.new(4, 2))
     end
-    @player_location = Coord.new(4,2)
-    place_player(@player_location)
   end
 
   def clone
     Marshal.load(Marshal.dump(self))
   end
 
-  def move_moving_object(moving_object, new_coord)
-    if moving_object.coord != nil
-      @hexArray[moving_object.coord.x][moving_object.coord.y] = nil
-    end
-    moving_object.coord = new_coord
-    @hexArray[moving_object.coord.x][moving_object.coord.y].contained_object = moving_object
+  def get_hex_at(coord)
+    @hexArray[coord.x][coord.y]
   end
 
-  def player
-    @hexArray[@player_location.x][@player_location.y].contained_object
+  def move_moving_object(moving_object, new_coord)
+    remove_object(moving_object)
+    place_object(moving_object, new_coord)
+  end
+
+  def place_object(moving_object, new_coord_or_hex)
+    hex = new_coord_or_hex
+    hex = get_hex_at(new_coord_or_hex) unless hex.is_a?(Hex)
+
+    moving_object.containing_hex = hex
+    moving_object.containing_hex.contained_object = moving_object
+  end
+
+  def remove_object(moving_object)
+    moving_object.containing_hex.contained_object = nil unless moving_object.containing_hex.nil?
   end
 
   def place_player(coord)
-    move_moving_object(MovingObject.new(MovingObject::PLAYER_TYPE), coord)
-    @player_location = coord
+    move_moving_object(@player, coord)
   end
 
   def to_s
     @hexArray.each do | line |
-      print "     " * (12 - line.size)
+      print "      " * (12 - line.size)
       line.each do | hex |
         print hex
-        print "     "
+        print "      "
       end
       puts
     end
   end
 
-  def neighbors(coords)
-    x,y = coords.x, coords.y
+  def show_image
+    temp_floor_tile = Image.new('tiles\tile_floor_1.png')
+    hex_width = temp_floor_tile.width - 6
+    hex_height = temp_floor_tile.width - 4
+    temp_floor_tile.remove
+    @hexArray.each_with_index do | line, x |
+      bottom_y = hex_height * 11
+      y_start = bottom_y - (11 - line.size) * (hex_height / 2)
+      line.each_with_index do | hex, y |
+        cur_x = x * hex_width
+        cur_y = y_start - y * hex_height
+
+        case hex.type
+        when Hex::FLOOR_TYPE
+          Image.new('tiles\tile_floor_1.png', x:cur_x, y:cur_y)
+        when Hex::LAVA_TYPE
+          Image.new('tiles\tile_liquid_1.png', x:cur_x, y:cur_y)
+        when Hex::EXIT_TYPE
+          Image.new('tiles\tile_base.png', x:cur_x, y:cur_y)
+          Image.new('tiles\dung_ladderdown.png', x:cur_x, y:cur_y)
+        else
+          Image.new('tiles\tile_base.png', x:cur_x, y:cur_y)
+        end
+        if hex.contained_object != nil && hex.contained_object.type == MovingObject::PLAYER_TYPE
+          Image.new('tiles\player.png', x:cur_x, y:cur_y)
+        end
+      end
+    end
+  end
+
+  def neighbors(coord)
+    x,y = coord.x, coord.y
     neighbor_coords = [Coord.new(x, y+1), Coord.new(x, y-1)]
     [-1,1].each do | x_off |
       neighbor_x = x + x_off
@@ -123,12 +186,12 @@ class Position
     neighbor_coords.select do | coord |
       coord.x >= 0 && coord.x < @hexArray.size && coord.y >= 0 && coord.y <= @hexArray[coord.x].size
     end.map do | neighbor_coord |
-      @hexArray[neighbor_coord.x][neighbor_coord.y]
+      get_hex_at(neighbor_coord)
     end
   end
 
   def playerNeighbors
-    neighbors(@player_location)
+    neighbors(@player.containing_hex.coord)
   end
 
   def generatePotentialPositions
@@ -136,16 +199,23 @@ class Position
   end
 end
 
-startingPosition = Position.new
-# puts startingPosition
-# puts
-# puts startingPosition.generatePotentialPositions
-# puts
-Step.getPossibleMoves(startingPosition).each do | possible_pos |
-  puts possible_pos
-  puts; puts
+
+board_dsl=(7..10).map{|i| "F"*i } + ["FPLFFFFFFEF"] + (7..10).reverse_each.map{|i| "F"*i }
+startingPosition = Position.new board_dsl
+startingPosition.show_image
+
+temp_possible_moves = Step.getPossibleMoves(startingPosition)
+
+on :key_down do
+  next_possible_move = temp_possible_moves.shift
+  if next_possible_move
+    next_possible_move.show_image
+  else
+    close
+  end
 end
 
+show
 
 def notMiniMax(startingPosition, depth)
   startingPosition.generatePotentialPositions.map do | potentialPosition, score |
